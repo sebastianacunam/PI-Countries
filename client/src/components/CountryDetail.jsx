baf5b7f05880a57e062ad1d8e551dd97653499ca
sebasjam <sebasjam96@gmail.com>
2022-04-25 01:16:59 -0300

    adelantos

--- client/src/components/CountryDetail.jsx
+++ client/src/components/CountryDetail.jsx
@@ -4,60 +4,67 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getDetail } from "../actions";
 import { useEffect } from "react";
+import estilos from './CountryDetail.module.css'
 
-export default function Detail (props){
-    const dispatch = useDispatch()
+export default function Detail(props) {
+  const dispatch = useDispatch()
 
-    useEffect(()=>{
-        dispatch(getDetail(props.match.params.id))
-    },[dispatch])
+  useEffect(() => {
+    dispatch(getDetail(props.match.params.id))
+  }, [dispatch])
 
-    const theCountry = useSelector((state)=> state.detail)
-    console.log(theCountry)
-    return (
-        <div>
-            {
-                theCountry.name ? 
-                <div>
-                  <img src={theCountry.flagImg} alt="Not found" />
-        
-                  <div>
-                    <h1 key={theCountry.name}>
-                      {theCountry.name} ({theCountry.id})
-                    </h1>
-                  </div>
-                  <div>
-                    <label>Capital</label>
-                    <h3 key={theCountry.capital}>{theCountry.capital}</h3>
-                  </div>
-                  <div>
-                    <label>Continente</label>
-                    <h2 key={theCountry.region}>
-                      {theCountry.region}
-                    </h2>
-                  </div>
-                  <div>
-                    <label>Subregion</label>
-                    <h3 key={theCountry.subregion}>
-                      {theCountry.subregion}
-                    </h3>
-                  </div>
-                  <div>
-                    <label>Area</label>
-                    <h3 key={theCountry.area}>
-                      {theCountry.area} km2
-                    </h3>
-                  </div>
-                  <div>
-                    <label >Poblacion</label>
-                    <h3 key={theCountry.population}>
-                      {theCountry.population}
-                    </h3>
-                  </div>
-                </div>
-              :
-              <p>Loading...</p>
-            }
+  const theCountry = useSelector((state) => state.detail)
+  console.log(theCountry)
+  return (
+
+    <div className={estilos.background}>
+      <section className={estilos.top}>
+        <Link to="/home">
+          <button className={estilos.volver}> Volver </button>
+        </Link>
+      </section>
+      {
+        theCountry.name ?
+          <div>
+            <img src={theCountry.flagImg} alt="Not found" />
+
+            <div>
+              <h1 key={theCountry.name}>
+                {theCountry.name} ({theCountry.id})
+              </h1>
+            </div>
+            <div>
+              <label>Capital</label>
+              <h3 key={theCountry.capital}>{theCountry.capital}</h3>
+            </div>
+            <div>
+              <label>Continente</label>
+              <h2 key={theCountry.region}>
+                {theCountry.region}
+              </h2>
+            </div>
+            <div>
+              <label>Subregion</label>
+              <h3 key={theCountry.subregion}>
+                {theCountry.subregion}
+              </h3>
+            </div>
+            <div>
+              <label>Area</label>
+              <h3 key={theCountry.area}>
+                {theCountry.area} km2
+              </h3>
+            </div>
+            <div>
+              <label >Poblacion</label>
+              <h3 key={theCountry.population}>
+                {theCountry.population}
+              </h3>
+            </div>
+          </div>
+          :
+          <p>Loading...</p>
+      }
 
       {theCountry.name && (
         <div >
@@ -86,12 +93,6 @@ export default function Detail (props){
           </div>
         </div>
       )}
-
-
-
-            <Link to="/home">
-              <button> Volver </button>
-            </Link>
-        </div>
-    )
+    </div>
+  )
 }   
