3440589af6bf3386787df987aecfce35a761ed58
sebastianacunam <61118493+sebastianacunam@users.noreply.github.com>
2022-04-26 15:11:13 -0300

    done

--- api/tests/routes/country.spec.js
+++ api/tests/routes/country.spec.js
@@ -2,7 +2,7 @@
 const { expect } = require('chai');
 const session = require('supertest-session');
 const app = require('../../src/app.js');
-const { Country, conn } = require('../../src/db.js');
+const { Country, Activity, conn } = require('../../src/db.js');
 
 const agent = session(app);
 const country = {
@@ -15,7 +15,7 @@ describe('Country routes', () => {
     console.error('Unable to connect to the database:', err);
   }));
   beforeEach(() => Country.sync({ force: true })
-    .then(() => Country.create(pokemon)));
+    .then(() => Country.create(Activity)));
   describe('GET /countries', () => {
     it('should get 200', () =>
       agent.get('/countries').expect(200)
