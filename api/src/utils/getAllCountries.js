2914a3b5fc0fddea8133481248acade9c41b76c2
sebastianacunam <61118493+sebastianacunam@users.noreply.github.com>
2023-11-15 14:27:40 -0300

    deploy en vercel

--- api/src/utils/getAllCountries.js
+++ api/src/utils/getAllCountries.js
@@ -8,6 +8,7 @@ const getAllCountries = async (req, res) => {
     if (!allCountries.length){
         const result = await CountriesDB();
         res.json(result)
+        
     } else {
         if (name) {           
             let countryName = await allCountries.filter( c => c.name.toLowerCase().includes(name.toLowerCase()))
@@ -15,6 +16,7 @@ const getAllCountries = async (req, res) => {
             console.log("toy aka")
         }
         else res.json(allCountries);
+        
     }   
     }
     catch(err){
