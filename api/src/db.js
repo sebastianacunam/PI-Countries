7ce2c55e5afd1eaa414bb58307d546cdb2b173cf
sebastianacunam <sebasjam96@gmail.com>
2025-01-27 16:31:09 -0300

    fix para produccion, @sebastianacunam

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-
 const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DATABASE_URL, NODE_ENV,
 } = process.env;
@@ -28,35 +27,40 @@ const sequelize =
 const basename = path.basename(__filename);
 const modelDefiners = [];
 
-// Leemos todos los archivos de la carpeta Models
+
+// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
 fs.readdirSync(path.join(__dirname, '/models'))
-  .filter(
-    (file) =>
-      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
-  )
+  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
     modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
 
-// Injectamos la conexión (sequelize) a todos los modelos
-modelDefiners.forEach((model) => model(sequelize));
-
-// Capitalizamos los nombres de los modelos
+// Injectamos la conexion (sequelize) a todos los modelos
+modelDefiners.forEach(model => model(sequelize));
+// Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map(([key, value]) => [
-  key[0].toUpperCase() + key.slice(1),
-  value,
-]);
+let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-// Relacionamos los modelos
-const { Recipe, Diet } = sequelize.models;
+// En sequelize.models están todos los modelos importados como propiedades
+// Para relacionarlos hacemos un destructuring
 
-Recipe.belongsToMany(Diet, { through: 'recipe_diet' });
-Diet.belongsToMany(Recipe, { through: 'recipe_diet' });
+const { Country, Activity } = sequelize.models;   // decia Pokemon cuando me fue entregado. 
 
-// Exportamos los modelos y la conexión
+// Aca vendrian las relaciones
+// Product.hasMany(Reviews);
+
+Country.belongsToMany(Activity, { through: 'country_activity' })
+Activity.belongsToMany(Country, { through: 'country_activity' })
+
+
 module.exports = {
-  ...sequelize.models, // para importar los modelos
-  conn: sequelize, // para importar la conexión
+  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
 };
+
+
+
+
+
+// configuracion de la base de datos 
